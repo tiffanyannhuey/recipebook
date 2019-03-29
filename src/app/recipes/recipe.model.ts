2a546ac7a74fb14fa58044d738bb226ed50a9185
tiffanyannhuey <87tiffanyhuey@gmail.com>
2019-03-28 21:05:36 -0700

    add recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: name, description: description, imagePath: imagePath) {
+    this.name = name;
+    this.description: description;
+    this.imagePath: imagePath;
+  }
+}
